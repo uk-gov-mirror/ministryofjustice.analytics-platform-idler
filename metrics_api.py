841bcc726b7b0eb046c02405e4251de9dfe69cd9
Andy Driver <andy@pagezero.net>
2018-03-16 12:16:12 +0000

    Factor out common code in Metrics API models

--- metrics_api.py
+++ metrics_api.py
@@ -72,7 +72,47 @@ kubernetes.client.apis.MetricsV1beta1Api = MetricsV1beta1Api
 kubernetes.client.MetricsV1beta1Api = MetricsV1beta1Api
 
 
-class MetricsV1beta1PodMetricsList(object):
+class Model(object):
+
+    def to_dict(self):
+        result = {}
+        for attr, _ in iteritems(self.swagger_types):
+            value = getattr(self, attr)
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: x.to_dict() if hasattr(x, 'to_dict') else x,
+                    value
+                ))
+            elif hasattr(value, 'to_dict'):
+                result[attr] = value.to_dict()
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], item[1].to_dict())
+                    if hasattr(item[1], 'to_dict') else item,
+                    value.items()
+                ))
+            else:
+                result[attr] = value
+
+        return result
+
+    def to_str(self):
+        return pformat(self.to_dict())
+
+    def __repr__(self):
+        return self.to_str()
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
+
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        return not self == other
+
+
+class MetricsV1beta1PodMetricsList(Model):
     swagger_types = {
         'api_version': 'str',
         'items': 'list[MetricsV1beta1PodMetrics]',
@@ -94,50 +134,13 @@ class MetricsV1beta1PodMetricsList(object):
         self.metadata = metadata
         self.discriminator = None
 
-    def to_dict(self):
-        result = {}
-        for attr, _ in iteritems(self.swagger_types):
-            value = getattr(self, attr)
-            if isinstance(value, list):
-                result[attr] = list(map(
-                    lambda x: x.to_dict() if hasattr(x, 'to_dict') else x,
-                    value
-                ))
-            elif hasattr(value, 'to_dict'):
-                result[attr] = value.to_dict()
-            elif isinstance(value, dict):
-                result[attr] = dict(map(
-                    lambda item: (item[0], item[1].to_dict())
-                    if hasattr(item[1], 'to_dict') else item,
-                    value.items()
-                ))
-            else:
-                result[attr] = value
-
-        return result
-
-    def to_str(self):
-        return pformat(self.to_dict())
-
-    def __repr__(self):
-        return self.to_str()
-
-    def __eq__(self, other):
-        if not isinstance(other, MetricsV1beta1PodMetricsList):
-            return False
-
-        return self.__dict__ == other.__dict__
-
-    def __ne__(self, other):
-        return not self == other
-
 
 kubernetes.client.models.MetricsV1beta1PodMetricsList = \
     MetricsV1beta1PodMetricsList
 kubernetes.client.MetricsV1beta1PodMetricsList = MetricsV1beta1PodMetricsList
 
 
-class MetricsV1beta1PodMetrics(object):
+class MetricsV1beta1PodMetrics(Model):
     swagger_types = {
         'containers': 'list[MetricsV1beta1ContainerMetrics]',
         'metadata': 'V1ObjectMeta',
@@ -159,48 +162,11 @@ class MetricsV1beta1PodMetrics(object):
         self.window = window
         self.discriminator = None
 
-    def to_dict(self):
-        result = {}
-        for attr, _ in iteritems(self.swagger_types):
-            value = getattr(self, attr)
-            if isinstance(value, list):
-                result[attr] = list(map(
-                    lambda x: x.to_dict() if hasattr(x, 'to_dict') else x,
-                    value
-                ))
-            elif hasattr(value, 'to_dict'):
-                result[attr] = value.to_dict()
-            elif isinstance(value, dict):
-                result[attr] = dict(map(
-                    lambda item: (item[0], item[1].to_dict())
-                    if hasattr(item[1], 'to_dict') else item,
-                    value.items()
-                ))
-            else:
-                result[attr] = value
-
-        return result
-
-    def to_str(self):
-        return pformat(self.to_dict())
-
-    def __repr__(self):
-        return self.to_str()
-
-    def __eq__(self, other):
-        if not isinstance(other, MetricsV1beta1PodMetrics):
-            return False
-
-        return self.__dict__ == other.__dict__
-
-    def __ne__(self, other):
-        return not self == other
-
 kubernetes.client.models.MetricsV1beta1PodMetrics = MetricsV1beta1PodMetrics
 kubernetes.client.MetricsV1beta1PodMetrics = MetricsV1beta1PodMetrics
 
 
-class MetricsV1beta1ContainerMetrics(object):
+class MetricsV1beta1ContainerMetrics(Model):
     swagger_types = {
         'name': 'str',
         'usage': 'dict(str, str)',
@@ -216,43 +182,5 @@ class MetricsV1beta1ContainerMetrics(object):
         self.usage = usage
         self.discriminator = None
 
-    def to_dict(self):
-        result = {}
-        for attr, _ in iteritems(self.swagger_types):
-            value = getattr(self, attr)
-            if isinstance(value, list):
-                result[attr] = list(map(
-                    lambda x: x.to_dict() if hasattr(x, 'to_dict') else x,
-                    value
-                ))
-            elif hasattr(value, 'to_dict'):
-                result[attr] = value.to_dict()
-            elif isinstance(value, dict):
-                result[attr] = dict(map(
-                    lambda item: (item[0], item[1].to_dict())
-                    if hasattr(item[1], 'to_dict') else item,
-                    value.items()
-                ))
-            else:
-                result[attr] = value
-
-        return result
-
-    def to_str(self):
-        return pformat(self.to_dict())
-
-    def __repr__(self):
-        return self.to_str()
-
-    def __eq__(self, other):
-        if not isinstance(other, MetricsV1beta1ContainerMetrics):
-            return False
-
-        return self.__dict__ == other.__dict__
-
-    def __ne__(self, other):
-        return not self == other
-
 kubernetes.client.models.MetricsV1beta1ContainerMetrics = MetricsV1beta1ContainerMetrics
 kubernetes.client.MetricsV1beta1ContainerMetrics = MetricsV1beta1ContainerMetrics
-
